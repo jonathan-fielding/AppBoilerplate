97da0444a37f687015c77f828da41dfddc6019ea
Jonathan Fielding <j.fielding@me.com>
2012-02-22 23:47:39 +0000

    added home module

---
+++ assets/js/core.js
@@ -0,0 +1,6 @@
+$('[data-role=page]').live('pageshow', function () {
+	var module_name = this.id;
+	
+	//Each module should have a javascript file, we pull this in here
+	require(['modules/'+module_name+'/'+ module_name +'.module.js']);
+});
