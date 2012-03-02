b08f71497fb9b431f8a65344126a7d2b361acece
Jonathan Fielding <j.fielding@me.com>
2012-03-02 16:53:10 +0000

    github
    github code

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
-/* Author:
+/* Author: Jonathan Fielding */
 
-*/
-
-
-
+$('.tab_box div').not(':first').hide();
+$('.tab_box menu a').click(function(){
+	var url = $(this).attr('href');
+	$('.tab_box div').hide();
+	$(url).show();
+	return false;
+});
 
 
