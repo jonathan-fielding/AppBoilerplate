f37de6e61077f3343816334f6d96e2aa82ca338e
Jonathan Fielding <j.fielding@me.com>
2012-03-07 08:10:56 +0000

    fixed the tabbox tabs
    Signed-off-by: Jonathan Fielding <j.fielding@me.com>

--- js/script.js
+++ js/script.js
@@ -2,10 +2,12 @@
 
 $('.tab_box div').not(':first').hide();
 $('.tab_box menu a').click(function(){
+	$('.tab_box menu a').removeClass('active');
+	$(this).addClass('active');
 	var url = $(this).attr('href');
 	$('.tab_box div').hide();
 	$(url).show();
 	return false;
-});
+}).first().addClass('active');
 
 
